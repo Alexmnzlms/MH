e0da434ab60617765f45509f249ef7cb15c2537b
Alexmnzlms <alexmanza2@gmail.com>
2020-03-04 19:20:31 +0100

    Con estas semillas funciona

--- Practicas/Practica1/src/main.cpp
+++ Practicas/Practica1/src/main.cpp
@@ -6,25 +6,25 @@ using namespace std;
 int main(){
    cout << "Ejecutando programa CCP" << endl;
    //par.mostrar_datos();
-   Set_random(987563214569);
-   /*for(int i = 0; i < 5; i++){
+   Set_random(20061999);
+   for(int i = 0; i < 5; i++){
       CCP par(3,"data/rand_set.dat","data/rand_set_const_10.const");
       //par.mostrar_datos();
-      cout << "-------------------Iteracion: " << i << "--------------------" << endl;
+      cout << "-------------------Iteracion: " << i+1 << "--------------------" << endl;
       par.greedy();
       par.mostrar_solucion(0);
       cout << "-------------------------------------------------------" << endl;
    }
-   //Set_random(20061999);
+   Set_random(03032020);
    for(int i = 0; i < 5; i++){
       CCP par(3,"data/iris_set.dat","data/iris_set_const_10.const");
       //par.mostrar_datos();
-      cout << "-------------------Iteracion: " << i << "--------------------" << endl;
+      cout << "-------------------Iteracion: " << i+1 << "--------------------" << endl;
       par.greedy();
       par.mostrar_solucion(0);
       cout << "-------------------------------------------------------" << endl;
    }
-   //Set_random(19031999);*/
+   Set_random(987563214569);
    for(int i = 0; i < 5; i++){
       CCP par(8,"data/ecoli_set.dat","data/ecoli_set_const_20.const");
       //par.mostrar_datos();
