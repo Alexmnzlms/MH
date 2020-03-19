d85c7e490a1e233bdfa414c7e2f63bf4f807fb9d
Alexmnzlms <alexmanza2@gmail.com>
2020-03-19 09:10:47 +0100

    Busqueda de semillas agregada a función

--- Practicas/Practica1/src/main.cpp
+++ Practicas/Practica1/src/main.cpp
@@ -4,6 +4,94 @@
 
 using namespace std;
 
+void buscar_semilla(){
+   bool salir = false;
+   bool fallo = false;
+   int n;
+   int n_max = 100;
+   unsigned seed;
+   while(!salir){
+      seed = std::time(0);
+      Set_random(seed);
+      cout << "Seed: " << seed << endl;
+      fallo = false;
+      for(int i = 0; i < 1 && !fallo; i++){
+         CCP par(8,"data/rand_set.dat","data/rand_set_const_10.const");
+         n = par.greedy();
+         if(n >= n_max){
+            fallo = true;
+            cout << "Fallo en la iteracion " << i+1 << " de rand 10%" << endl;
+         }
+         else{
+            cout << "Conseguida iteracion " << i+1 << " de rand 10%" << endl;
+         }
+      }
+      Set_random(seed);
+      for(int i = 0; i < 1 && !fallo; i++){
+         CCP par(8,"data/rand_set.dat","data/rand_set_const_20.const");
+         n = par.greedy();
+         if(n >= n_max){
+            fallo = true;
+            cout << "Fallo en la iteracion " << i+1 << " de rand 20%" << endl;
+         }
+         else{
+            cout << "Conseguida iteracion " << i+1 << " de rand 20%" << endl;
+         }
+      }
+      Set_random(seed);
+      for(int i = 0; i < 1 && !fallo; i++){
+         CCP par(8,"data/iris_set.dat","data/iris_set_const_10.const");
+         n = par.greedy();
+         if(n >= n_max){
+            fallo = true;
+            cout << "Fallo en la iteracion " << i+1 << " de iris 10%" << endl;
+         }
+         else{
+            cout << "Conseguida iteracion " << i+1 << " de iris 10%" << endl;
+         }
+      }
+      Set_random(seed);
+      for(int i = 0; i < 1 && !fallo; i++){
+         CCP par(8,"data/iris_set.dat","data/iris_set_const_20.const");
+         n = par.greedy();
+         if(n >= n_max){
+            fallo = true;
+            cout << "Fallo en la iteracion " << i+1 << " de iris 20%" << endl;
+         }
+         else{
+            cout << "Conseguida iteracion " << i+1 << " de iris 20%" << endl;
+         }
+      }
+      Set_random(seed);
+      for(int i = 0; i < 1 && !fallo; i++){
+         CCP par(8,"data/ecoli_set.dat","data/ecoli_set_const_10.const");
+         n = par.greedy();
+         if(n >= n_max){
+            fallo = true;
+            cout << "Fallo en la iteracion " << i+1 << " de ecoli 10%" << endl;
+         }
+         else{
+            cout << "Conseguida iteracion " << i+1 << " de ecoli 10%" << endl;
+         }
+      }
+      Set_random(seed);
+      for(int i = 0; i < 5 && !fallo; i++){
+         CCP par(1,"data/ecoli_set.dat","data/ecoli_set_const_20.const");
+         n = par.greedy();
+         if(n >= n_max){
+            fallo = true;
+            cout << "Fallo en la iteracion " << i+1 << " de ecoli 20%" << endl;
+         }
+         else{
+            cout << "Conseguida iteracion " << i+1 << " de ecoli 20%" << endl;
+         }
+      }
+      if(!fallo){
+         salir = true;
+      }
+   }
+}
+
 int main(){
    unsigned long tini, tfin;
    double semillas [5] = {1584565171,1584764782,1584565259,1584564539,1522565615};
@@ -180,112 +268,4 @@ int main(){
       par.mostrar_solucion();
       cout << "-------------------------------------------------------" << endl;
    }
-
-/*
-   bool salir = false;
-   bool fallo = false;
-   int n;
-   int n_max = 100;
-   unsigned seed;
-   while(!salir){
-      seed = std::time(0);
-      Set_random(seed);
-      cout << "Seed: " << seed << endl;
-      //cout << "Ecoli_set 10%" << endl;
-      fallo = false;
-      for(int i = 0; i < 1 && !fallo; i++){
-         CCP par(8,"data/rand_set.dat","data/rand_set_const_10.const");
-         //par.mostrar_datos();
-         //cout << "-------------------Iteracion: " << i+1 << "--------------------" << endl;
-         n = par.greedy();
-         if(n >= n_max){
-            fallo = true;
-            cout << "Fallo en la iteracion " << i+1 << " de rand 10%" << endl;
-         }
-         else{
-            cout << "Conseguida iteracion " << i+1 << " de rand 10%" << endl;
-         }
-         //par.mostrar_solucion();
-         //cout << "-------------------------------------------------------" << endl;
-      }
-      for(int i = 0; i < 1 && !fallo; i++){
-         CCP par(8,"data/rand_set.dat","data/rand_set_const_20.const");
-         //par.mostrar_datos();
-         //cout << "-------------------Iteracion: " << i+1 << "--------------------" << endl;
-         n = par.greedy();
-         if(n >= n_max){
-            fallo = true;
-            cout << "Fallo en la iteracion " << i+1 << " de rand 20%" << endl;
-         }
-         else{
-            cout << "Conseguida iteracion " << i+1 << " de rand 20%" << endl;
-         }
-         //par.mostrar_solucion();
-         //cout << "-------------------------------------------------------" << endl;
-      }
-      for(int i = 0; i < 1 && !fallo; i++){
-         CCP par(8,"data/iris_set.dat","data/iris_set_const_10.const");
-         //par.mostrar_datos();
-         //cout << "-------------------Iteracion: " << i+1 << "--------------------" << endl;
-         n = par.greedy();
-         if(n >= n_max){
-            fallo = true;
-            cout << "Fallo en la iteracion " << i+1 << " de iris 10%" << endl;
-         }
-         else{
-            cout << "Conseguida iteracion " << i+1 << " de iris 10%" << endl;
-         }
-         //par.mostrar_solucion();
-         //cout << "-------------------------------------------------------" << endl;
-      }
-      for(int i = 0; i < 1 && !fallo; i++){
-         CCP par(8,"data/iris_set.dat","data/iris_set_const_20.const");
-         //par.mostrar_datos();
-         //cout << "-------------------Iteracion: " << i+1 << "--------------------" << endl;
-         n = par.greedy();
-         if(n >= n_max){
-            fallo = true;
-            cout << "Fallo en la iteracion " << i+1 << " de iris 20%" << endl;
-         }
-         else{
-            cout << "Conseguida iteracion " << i+1 << " de iris 20%" << endl;
-         }
-         //par.mostrar_solucion();
-         //cout << "-------------------------------------------------------" << endl;
-      }
-      for(int i = 0; i < 1 && !fallo; i++){
-         CCP par(8,"data/ecoli_set.dat","data/ecoli_set_const_10.const");
-         //par.mostrar_datos();
-         //cout << "-------------------Iteracion: " << i+1 << "--------------------" << endl;
-         n = par.greedy();
-         if(n >= n_max){
-            fallo = true;
-            cout << "Fallo en la iteracion " << i+1 << " de ecoli 10%" << endl;
-         }
-         else{
-            cout << "Conseguida iteracion " << i+1 << " de ecoli 10%" << endl;
-         }
-         //par.mostrar_solucion();
-         //cout << "-------------------------------------------------------" << endl;
-      }
-      for(int i = 0; i < 5 && !fallo; i++){
-         CCP par(1,"data/ecoli_set.dat","data/ecoli_set_const_20.const");
-         //par.mostrar_datos();
-         //cout << "-------------------Iteracion: " << i+1 << "--------------------" << endl;
-         n = par.greedy();
-         if(n >= n_max){
-            fallo = true;
-            cout << "Fallo en la iteracion " << i+1 << " de ecoli 20%" << endl;
-         }
-         else{
-            cout << "Conseguida iteracion " << i+1 << " de ecoli 20%" << endl;
-         }
-         //par.mostrar_solucion();
-         //cout << "-------------------------------------------------------" << endl;
-      }
-      if(!fallo){
-         salir = true;
-      }
-   }
-*/
 }
